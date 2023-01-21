e522cf7069ff14e908f58fb9399a865cfa13ab9a
Maksym-Palii <110907706+Maksym-Palii@users.noreply.github.com>
2023-01-21 17:54:07 +0000

    Deploying to gh-pages from @ Maksym-Palii/goit-js-hw-09@65e6cca57412353d2af84ded51a4b8ac66c8148f 🚀

---
+++ 01-color-switcher.1088462f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1})),e.disabled=!0;var a=null}();
+//# sourceMappingURL=01-color-switcher.1088462f.js.map
