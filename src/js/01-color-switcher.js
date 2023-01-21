65e6cca57412353d2af84ded51a4b8ac66c8148f
Maksym-Palii <paliym_@ukr.net>
2023-01-21 19:53:38 +0200

    js-hw-09-var-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStoptEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStartEl.addEventListener('click', startChangeColor);
+btnStoptEl.addEventListener('click', stopChangeColor);
+
+btnStoptEl.disabled = true;
+
+const INTERVAL_DELAY = 1000;
+let timerId = null;
+
+function startChangeColor() {
+  function bgColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }
+  timerId = setInterval(bgColor, INTERVAL_DELAY);
+
+  btnStartEl.disabled = true;
+  btnStoptEl.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  btnStoptEl.disabled = true;
+  btnStartEl.disabled = false;
+}
